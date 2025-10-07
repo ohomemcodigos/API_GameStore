161af6605e1a808b09a07fc1a7296c7d0ef929d2
Guilherme <guittrex.2005@gmail.com>
2025-10-06 21:17:39 -0300

    Instalando o Swagger no projeto.  Implementado o Swagger para documentação e refatorado a rota de jogos.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 //Importando
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import { swaggerOptions } from './swaggerOptions'; // Nossas opções
 
 //Importação das Rotas
-/*import gameRoutes from './routes/gameRoutes';
-import userRoutes from './routes/userRoutes';
-import purchaseRoutes from './routes/purchaseRoutes';*/
-//Comentário para testes
+import gameRoutes from './routes/gameRoutes';
+//import userRoutes from './routes/userRoutes';
+//import purchaseRoutes from './routes/purchaseRoutes';
 
 //Inicialização do app e do Prisma
 const app = express();
@@ -16,17 +18,21 @@ export const prisma = new PrismaClient();
 //Middlewares
 app.use(express.json()); // Middleware para interpretar JSON
 
+// --- Configuração do Swagger ---
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 //Rotas
-/*app.get('/', (req, res) => {
-  res.send('API da Loja de Jogos está no ar!');
+app.get('/', (req, res) => {
+  res.send('API da Loja de Jogos está no ar!');
 });
 
+
 app.use('/api/games', gameRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/purchases', purchaseRoutes);*/
-//Comentário para testes
+//app.use('/api/users', userRoutes);
+//app.use('/api/purchases', purchaseRoutes);
 
 //Inicia o servidor
 app.listen(port, () => {
-  console.log(`Servidor ligou! Ele está rodando em http://localhost:${port}`);
+  console.log(`Servidor ligou! Ele está rodando em http://localhost:${port}`);
 });
